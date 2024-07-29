68ae28cf4fbfe7aa724cd5a55359c8d647ef102f
Vlad Petrariu <vlad.petrariu7@gmail.com>
2024-07-29 15:44:09 -0700

    Black game panel created

--- chessGame/src/main/Main.java
+++ chessGame/src/main/Main.java
@@ -1,9 +1,23 @@
 package main;
 
+import javax.swing.JFrame;
+
 public class Main {
 	
 	public static void main(String[]args) {
 		
+		JFrame window = new JFrame("chessGame");
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
+		window.setResizable(false);
+		
+		//add game panel to the window
+		gamePanel gp = new gamePanel();
+		window.add(gp);
+		window.pack();
+		
+		
+		window.setLocationRelativeTo(null);
+		window.setVisible(true);
 		
 		
 	}
